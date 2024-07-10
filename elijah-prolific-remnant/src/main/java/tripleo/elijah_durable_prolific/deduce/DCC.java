14351bdae81e00a7827f20ec8b6cfb92e1edf2c8
tripleo <tripleo@tripleo.milkyway>
2024-07-10 17:02:36 -0400

    020: Change order, add ErrSink

--- elijah-prolific-remnant/src/main/java/tripleo/elijah_durable_prolific/deduce/DCC.java
+++ elijah-prolific-remnant/src/main/java/tripleo/elijah_durable_prolific/deduce/DCC.java
@@ -1,10 +1,12 @@
 package tripleo.elijah_durable_prolific.deduce;
 
+import tripleo.elijah.comp.*;
 import tripleo.elijah.stages.deduce.*;
 
 public record DCC(
 		DeduceTypes2 deduceTypes2,
-		DeducePhase deducePhase
+		DeducePhase deducePhase,
+		ErrSink errSink
 ) {
 	public DeducePhase phase() {
 		return this.deducePhase;
