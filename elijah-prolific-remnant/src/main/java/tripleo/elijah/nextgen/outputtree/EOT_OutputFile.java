522f0d6e8ad64181c1ea4bef99494b5988d28e9e
tripleo <tripleo@tripleo.milkyway>
2024-07-12 02:01:01 -0400

    3:

--- elijah-prolific-remnant/src/main/java/tripleo/elijah/nextgen/outputtree/EOT_OutputFile.java
+++ elijah-prolific-remnant/src/main/java/tripleo/elijah/nextgen/outputtree/EOT_OutputFile.java
@@ -30,29 +30,6 @@ public class EOT_OutputFile {
 		_inputs.addAll(inputs);
 	}
 
-	/*
-	 * public static EOT_OutputFile grToOutputFile(final Compilation aC, final
-	 * GenerateResultItem ab) { final List<EIT_Input> inputs = List_of(new
-	 * EIT_ModuleInput(ab.node.module(), aC));
-	 * 
-	 * final EG_SingleStatement beginning = new EG_SingleStatement("", new
-	 * EX_Explanation() { }); final EG_SingleStatement middle = new
-	 * EG_SingleStatement(ab.buffer.getText(), new EX_Explanation() { }); final
-	 * EG_SingleStatement ending = new EG_SingleStatement("", new EX_Explanation() {
-	 * }); final EX_Explanation explanation = new EX_Explanation() { public String
-	 * getText() { return "generate-result-item"; } };
-	 * 
-	 * final EG_CompoundStatement seq = new EG_CompoundStatement(beginning, ending,
-	 * middle, false, explanation);
-	 * 
-	 * final EOT_OutputFile eof = new EOT_OutputFile(aC, inputs, ab.output,
-	 * EOT_OutputType.SOURCES, seq);
-	 * 
-	 * aC.reports().addCodeOutput(()->ab.output, eof);
-	 * 
-	 * return eof; }
-	 */
-
 	public static @NotNull EOT_OutputFile bufferSetToOutputFile(final String aFilename,
 			final @NotNull Collection<Buffer> aBuffers, final Compilation comp, final OS_Module aModule) {
 		final List<EIT_Input> inputs = List_of(new EIT_ModuleInput(aModule, comp));
@@ -68,11 +45,6 @@ public class EOT_OutputFile {
 
 		return eof;
 	}
-
-	/*
-	 * public EOT_OutputFile(final OS_Module module, final Compilation c) {
-	 * this.module = module; this.c = c; }
-	 */
 
 	public String getFilename() {
 		return _filename;
--- elijah-prolific-remnant/src/main/java/tripleo/elijah_prolific/v/V.java
+++ elijah-prolific-remnant/src/main/java/tripleo/elijah_prolific/v/V.java
@@ -1,6 +1,9 @@
 package tripleo.elijah_prolific.v;
 
 import tripleo.elijah.stages.gen_generic.*;
+import com.google.gson.*;
+import org.apache.commons.lang3.tuple.*;
+import tripleo.elijah.ci.*;
 import tripleo.elijah.comp.*;
 import tripleo.elijah.nextgen.inputtree.*;
 import tripleo.elijah.nextgen.outputstatement.*;
@@ -10,6 +13,7 @@ import tripleo.elijah.stages.gen_generic.*;
 import java.io.*;
 import java.util.*;
 import java.io.*;
+import java.text.*;
 import java.util.*;
 import java.util.stream.*;
 
@@ -17,10 +21,13 @@ import static tripleo.elijah_fluffy.util.Helpers.List_of;
 
 public class V {
 	private static final List<String> logs = new ArrayList<>();
+	private static List<_JsonLog> jsonLogs = new ArrayList<>();
 
 	public static void asv(final e aE, final String aKey) {
 		final String x = "{{V.asv}} " + aE + " " + aKey;
 		addLog(x);
+		addJsonLog("asv", ""+aE, List_of(aKey));
+		//System.err.println("[debug] "+x);
 	}
 
 	public static void gri(final GenerateResult gr) {
@@ -38,21 +45,52 @@ public class V {
 //			stream.println(bt);
 			final String x = "{{V.gr}} " + ty + " " + ou + " " + ns;
 			addLog(x);
+			addJsonLog("gr", "", List_of(ty, ou, ns));
 		}
 	}
 
 	public static void exit(Compilation c) {
 		final String x = "{{V.exit}}";
 		addLog(x);
+		addJsonLog("exit", "", List_of(""));
 
-		{
-			final List<EIT_Input>                     inputs          = List_of();
-			final List<EG_Statement>                  sts             = logs.stream().map(EG_SingleStatement::new).collect(Collectors.toList());
-			final EG_SequenceFactory._SequenceBuilder sequenceBuilder = EG_SequenceFactory.newSequence().addParts(sts);
-			final EG_Statement                        seq             = sequenceBuilder.build();
-			final EOT_OutputFile                      off             = new EOT_OutputFile(c, inputs, "error-report.txt", EOT_OutputType.ERROR_REPORT, seq);
-			c.getOutputTree().add(off);
-		}
+		finishJsonLog(c);
+	}
+
+	public record _JsonLog(String headCode, String second, List<String> stringList) {}
+
+	public static class _JsonLog_TypeAdapter {}
+
+	private static void addJsonLog(final String aHeadCode, final String aSecond, final List<String> aStringList) {
+		jsonLogs.add(new _JsonLog(aHeadCode, aSecond, aStringList));
+	}
+
+	private static void finishJsonLog(final Compilation c) {
+		//final Gson gson = new Gson();
+		//final String jsonString  = gson.toJson(logs);
+
+		final Gson gson = new GsonBuilder()
+				.registerTypeAdapter(_JsonLog.class, new _JsonLog_TypeAdapter())
+				.enableComplexMapKeySerialization()
+				.serializeNulls()
+				.setDateFormat(DateFormat.LONG)
+				.setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+				.setPrettyPrinting()
+				.setVersion(1.0)
+				.create();
+		final String jsonString  = gson.toJson(jsonLogs);
+
+
+		final List<EIT_Input>                     inputs          = List_of();
+		final List<EG_Statement>                  sts             = logs.stream().map(EG_SingleStatement::new).collect(Collectors.toList());
+		final EG_SequenceFactory._SequenceBuilder sequenceBuilder = EG_SequenceFactory.newSequence().addParts(sts);
+		final EG_Statement                        seq             = sequenceBuilder.build();
+		final EOT_OutputFile                      off             = new EOT_OutputFile(c, inputs, "error-report.txt", EOT_OutputType.ERROR_REPORT, seq);
+		c.getOutputTree().add(off);
+
+		final EG_Statement                        seq2             = new EG_SingleStatement(jsonString);
+		final EOT_OutputFile                      off2             = new EOT_OutputFile(c, inputs, "error-report.json", EOT_OutputType.ERROR_REPORT, seq2);
+		c.getOutputTree().add(off2);
 	}
 
 	private static void addLog(final String x) {
