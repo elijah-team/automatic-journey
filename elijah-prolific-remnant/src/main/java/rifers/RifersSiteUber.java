bbac29b246e90a65e3e142bd60dae749d2723891
tripleo <tripleo@tripleo.milkyway>
2024-07-12 18:40:51 -0400

    Re-post later

--- elijah-prolific-remnant/src/main/java/rifers/RifersSiteUber.java
+++ elijah-prolific-remnant/src/main/java/rifers/RifersSiteUber.java
@@ -1,11 +1,11 @@
-package rifers;
-
-import rife.engine.*;
-
-public class RifersSiteUber extends RifersSite {
-    public static void main(String[] args) {
-        new Server()
-            .staticUberJarResourceBase("webapp")
-            .start(new RifersSiteUber());
-    }
-}
+//package rifers;
+//
+//import rife.engine.*;
+//
+//public class RifersSiteUber extends RifersSite {
+//    public static void main(String[] args) {
+//        new Server()
+//            .staticUberJarResourceBase("webapp")
+//            .start(new RifersSiteUber());
+//    }
+//}
