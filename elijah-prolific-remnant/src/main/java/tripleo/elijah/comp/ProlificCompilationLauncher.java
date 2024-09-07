f332cbf54ccbadecc107c47354d510062753aa82
Tripleo <noble@tripleo.nonexistent>
2024-09-07 13:53:35 -0400

    2024-09-07 017: fix: Need to manually create an Eventloop
    - TODO: Remove effusive logging ("Less Noise" principle)
    - TODO: Can we inject the reactor into the service?
    - TODO: Utilize ServiceGraph a litlle more
    - not TODO: make mw joke without bear

--- elijah-prolific-remnant/src/main/java/tripleo/elijah/comp/ProlificCompilationLauncher.java
+++ elijah-prolific-remnant/src/main/java/tripleo/elijah/comp/ProlificCompilationLauncher.java
@@ -1,21 +1,24 @@
 package tripleo.elijah.comp;
 
+import io.activej.eventloop.Eventloop;
 import io.activej.inject.annotation.Inject;
-import io.activej.inject.annotation.Provides;
+import io.activej.inject.module.Module;
 import io.activej.launcher.Launcher;
+import io.activej.service.Service;
+import io.activej.service.ServiceGraphModule;
+
+import java.util.concurrent.CompletableFuture;
+
 import org.jetbrains.annotations.NotNull;
+import java.util.List;
+
 import tripleo.elijah.comp.i.CompilerController;
 import tripleo.elijah_prolific.v.V;
-
-import java.util.List;
 
 class ProlificCompilationLauncher extends Launcher {
     private final @NotNull Compilation        compilation;
     private final @NotNull List<String>       args;
     private final @NotNull CompilerController controller;
-
-    @Inject
-    String message;
 
     public ProlificCompilationLauncher(final @NotNull Compilation aCompilation,
                                        final @NotNull List<String> aStringList,
@@ -25,22 +28,48 @@ class ProlificCompilationLauncher extends Launcher {
         controller  = aController;
     }
 
-    @Provides
-    String message() {
-        return "Hello, world!";
+    @Inject
+    CustomService customService;
+
+    @Override
+    protected Module getModule() {
+        return ServiceGraphModule.create();
+    }
+
+    @Inject
+    private class CustomService implements Service {
+        @Override
+        public CompletableFuture<?> start() {
+            logProgress("|SERVICE STARTING|");
+            {
+                controller.setInputs(compilation, args);
+                controller.processOptions();
+                controller.runner();
+
+                V.exit(compilation);
+            }
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public CompletableFuture<?> stop() {
+            logProgress("|SERVICE STOPPING|");
+            return CompletableFuture.completedFuture(null);
+        }
     }
 
     @Override
     protected void run() {
-        controller.setInputs(compilation, args);
-        controller.processOptions();
-        controller.runner();
+        logProgress("|RUNNING|");
+    }
 
-        V.exit(compilation);
+    private static void logProgress(final String x) {
+        System.out.println(x);
     }
 
     public void launch0() {
         try {
+            Eventloop.builder().withCurrentThread().build();
             var launcher = this;
             launcher.launch(new String[0]);
         } catch (Exception aE) {
